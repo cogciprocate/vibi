766ca0e002f5a7af041bb8b02d7d3238c9618ddb
Nick Sanders <cogciprocate@gmail.com>
2015-12-02 16:47:07 -0800

    Move prompt into its own function.

--- src/interactive/visualize.rs
+++ src/interactive/visualize.rs
@@ -144,7 +144,7 @@ pub fn define_pamaps() -> ProtoareaMaps {
 			- Or just be lazy and leave it the beautiful disaster that it is...	
 */
 pub fn run(autorun_iters: i32) -> bool {
-	#![allow(unused_assignments)]
+	#![allow(unused_assignments, dead_code)]
 
 	// KEEP UNTIL WE FIGURE OUT HOW TO GENERATE THIS NUMBER ELSEWHERE
 	let mut ir_labels_vec: Vec<u8> = Vec::with_capacity(1);
@@ -154,7 +154,7 @@ pub fn run(autorun_iters: i32) -> bool {
 		"data/train-labels-idx1-ubyte", CYCLES_PER_FRAME, 1.0);
 	
 	let mut cortex = cortex::Cortex::new(define_plmaps(), define_pamaps());
-	let mut area_name = "v1".to_string();
+	let area_name = "v1".to_string();
 	// let inhib_layer_name = "iv_inhib";
 	// let area_dims = cortex.area(&area_name).dims().clone();
 
@@ -194,221 +194,76 @@ pub fn run(autorun_iters: i32) -> bool {
 	//let mut vec_out_prev: Vec<u8> = iter::repeat(0).take(area_dims.columns() as usize).collect();
 	//let mut vec_ff_prev: Vec<u8> = iter::repeat(0).take(area_dims.columns() as usize).collect();
 
-	let mut test_iters: i32 = if autorun_iters > 0 {
-		autorun_iters
-	} else {
-		INITIAL_TEST_ITERATIONS
-	};
+	// let test_iters: i32 = if autorun_iters > 0 {
+	// 	autorun_iters
+	// } else {
+	// 	INITIAL_TEST_ITERATIONS
+	// };
 
-	let mut first_run: bool = true;
-	let mut bypass_act = false;
+	// let first_run: bool = true;
+	// let bypass_act = false;
 
-	let mut view_all_axons: bool = false;
-	let mut view_sdr_only: bool = true;
-	let mut cur_ttl_iters: i32 = 0;
+	// let view_all_axons: bool = false;
+	// let view_sdr_only: bool = true;
+	// let cur_ttl_iters: i32 = 0;
 	let mut input_status: String = String::with_capacity(100);
 
+	let mut ri = RunInfo {
+		cortex: cortex,
+		test_iters: if autorun_iters > 0 {
+				autorun_iters
+			} else {
+				INITIAL_TEST_ITERATIONS
+			}, 
+		bypass_act: false, 
+		autorun_iters: autorun_iters, 
+		first_run: true, 
+		view_all_axons: false, 
+		view_sdr_only: true,
+		area_name: area_name,
+		cur_ttl_iters: 0,
+	};
+
 	loop {
-		/*######### COMMAND LINE #########*/
-		if test_iters == 0 {
-			test_iters = 1;
-			bypass_act = true; 
-		} else {
-			bypass_act = false;
+		match prompt(&mut ri) {
+			LoopAction::Continue => continue,
+			LoopAction::Break => break,
+			LoopAction::None => (),
 		}
-
-		if autorun_iters == 0 {
-			let in_string: String = if first_run {
-				first_run = false;
-				"\n".to_string()
-			} else {
-				let axn_state = if view_all_axons { "on" } else { "off" };
-				let view_state = if view_sdr_only { "sdr" } else { "all" };
-
-				rin(format!("bismit: [{ttl_i}/({loop_i})]: [v]iew:[{}] [a]xons:[{}] \
-					[m]otor:[X] a[r]ea:[{}] [t]ests [q]uit [i]ters:[{iters}]", 
-					view_state, axn_state, /*input_czar.motor_state.cur_str(),*/ area_name, 
-					iters = test_iters,
-					loop_i = 0, //input_czar.counter(), 
-					ttl_i = cur_ttl_iters,
-				))
-			};
-
-
-			if "q\n" == in_string {
-				print!("\nExiting interactive test mode... ");
-				break;
-			} else if "i\n" == in_string {
-				let in_s = rin(format!("Iterations: [i={}]", test_iters));
-				if "\n" == in_s {
-					continue;
-					//test_iters = TEST_ITERATIONS;
-				} else {
-					let in_int: Option<i32> = parse_num(in_s);
-					match in_int {
-						Some(x)	=> {
-							 test_iters = x;
-							 //continue;
-						},
-						None    => {
-							print!("Invalid number.\n");
-							continue;
-						},
-					}
-				}
-
-			} else if "r\n" == in_string {
-				let in_str = rin(format!("area name"));
-				let in_s1 = in_str.trim();
-				let new_area_name = in_s1.to_string();
-				
-				if cortex.valid_area(&new_area_name) {
-					area_name = new_area_name;
-				} else {
-					print!("Invalid area.");
-				}
-				//continue;
-				bypass_act = true;
-
-			} else if "v\n" == in_string {
-				view_sdr_only = !view_sdr_only;
-				bypass_act = true;
-
-			} else if "\n" == in_string {
-				// DO NOT REMOVE
-
-			} else if "a\n" == in_string {
-				view_all_axons = !view_all_axons;
-				bypass_act = true;
-
-			} else if "t\n" == in_string {
-				// bypass_act = true;
-				let in_s = rin(format!("tests: [f]ract [c]ycles [l]earning [a]ctivate a[r]ea_output o[u]tput"));
-
-				if "p\n" == in_s {
-					//synapse_drill_down::print_pyrs(&mut cortex);
-					continue;
-
-				} else if "u\n" == in_s {
-					// let in_str = rin(format!("area name"));
-					// let in_s1 = in_str.trim();
-					// let out_len = cortex.area(&in_s).dims.columns();
-					// let t_vec: Vec<u8> = iter::repeat(0).take(out_len as usize).collect();
-					// cortex.area_mut(&in_s).read_output(&mut t_vec, map::FF_OUT);
-					// ocl::fmt::print_vec_simple(&t_vec);
-					println!("\n##### PRINTING TEMPORARILY DISABLED #####");
-					continue;
-
-				} else if "c\n" == in_s {
-					println!("\n##### DISABLED #####");
-					//hybrid::test_cycles(&mut cortex, &area_name);
-					continue;
-
-				} else if "l\n" == in_s {
-					println!("\n##### DISABLED #####");
-					//learning::test_learning_cell_range(&mut cortex, inhib_layer_name, &area_name);
-					continue;
-
-				} else if "a\n" == in_s {
-					println!("\n##### DISABLED #####");
-					//learning::test_learning_activation(&mut cortex, &area_name);
-					continue;
-
-				// } else if "f\n" == in_s {
-				// 	let in_s = rin(format!("fractal seed"));
-				// 	let in_int: Option<u8> = in_s.trim().parse().ok();
-
-				// 	// let seed = match in_int {
-				// 	// 	Some(x)	=> x,
-				// 	// 	None => {
-				// 	// 		print!("\nError parsing number.");
-				// 	// 		continue;
-				// 	// 	},
-				// 	// };
-
-				// 	let in_s = rin(format!("cardinality factor: 256 * "));
-				// 	let in_int: Option<usize> = in_s.trim().parse().ok();
-
-				// 	let c_factor = match in_int {
-				// 		Some(x)	=> x,
-				// 		None => {
-				// 			print!("\nError parsing number.");
-				// 			continue;
-				// 		},
-				// 	};
-
-				// 	// let tvec = cmn::gen_fract_sdr(seed, 256 * c_factor);
-				// 	// ocl::fmt::print_vec_simple(&tvec[..]);
-				// 	println!("\n##### PRINTING TEMPORARILY DISABLED #####");
-				// 	continue;
-
-				// } else if "r\n" == in_s {
-				// 	let in_str = rin(format!("area name"));
-				// 	// let in_s = in_str.trim();
-				// 	//let in_int: Option<u8> = in_s.trim().parse().ok();
-
-				// 	println!("\n##### DISABLED #####");
-				// 	//cortex.print_area_output(&in_s);
-				// 	continue;
-
-				} else {
-					continue;
-				}
-
-
-			} else if "m\n" == in_string {
-				// bypass_act = true;
-				let in_s = rin(format!("motor: [s]witch(disconnected)"));
-				if "s\n" == in_s {
-					//input_czar.motor_state.switch();
-					//println!("\nREPLACE ME - synapse_sources::run() - line 100ish");
-					continue;
-					//test_iters = TEST_ITERATIONS;
-
-				} else {
-					continue;
-				}
-			} else {
-				continue;
-			}
-		}
-
 
 		let time_start = time::get_time();
 
-		if test_iters > 1 {
-			print!("Running {} iterations... \n", test_iters);
+		if ri.test_iters > 1 {
+			print!("Running {} iterations... \n", ri.test_iters);
 		}
-
-
 
 		// CURRENT ACTIVE ITERATION
 		let mut i = 0i32;
 
 		/*######### SENSE ONLY LOOP #########*/
-		if !view_sdr_only { print!("\nRunning {} sense only loop(s) ... \n", test_iters - 1); }
+		if !ri.view_sdr_only { print!("\nRunning {} sense only loop(s) ... \n", ri.test_iters - 1); }
 
 		loop {
-			if i >= (test_iters - 1) { break; }
+			if i >= (ri.test_iters - 1) { break; }
 
-			if i % STATUS_EVERY == 0 || i < 0 || i == (test_iters - 2) {
+			if i % STATUS_EVERY == 0 || i < 0 || i == (ri.test_iters - 2) {
 				let t = time::get_time() - time_start;
-				if i > 0 || (test_iters > 1 && i == 0) {
+				if i > 0 || (ri.test_iters > 1 && i == 0) {
 					print!("[{}: {:02.4}ms]", i, t.num_milliseconds());
 				}
 				io::stdout().flush().ok();
 			}
 
 			if i % PRINT_DETAILS_EVERY == 0 || i < 0 {
-				if !view_sdr_only { 
-					output_czar::print_sense_only(&mut cortex, &area_name); 
+				if !ri.view_sdr_only { 
+					output_czar::print_sense_only(&mut ri.cortex, &ri.area_name); 
 				}
 			}
 						
-			if !bypass_act {
+			if !ri.bypass_act {
 				//input_czar.next(&mut cortex);
 				ir_labels.cycle(&mut ir_labels_vec[..]);
-				cortex.cycle();
+				ri.cortex.cycle();
 			}
 
 
@@ -418,38 +273,38 @@ pub fn run(autorun_iters: i32) -> bool {
 
 
 		/*######### SENSE AND PRINT LOOP #########*/
-		if !view_sdr_only { print!("\n\nRunning {} sense and print loop(s)...", 1usize); }
+		if !ri.view_sdr_only { print!("\n\nRunning {} sense and print loop(s)...", 1usize); }
 
 		loop {
-			if i >= (test_iters) { break; }
+			if i >= (ri.test_iters) { break; }
 
-			if !bypass_act {
+			if !ri.bypass_act {
 				//input_czar.next(&mut cortex); // Just increments counter
 				ir_labels.cycle(&mut ir_labels_vec[..]);
-				cortex.cycle();
+				ri.cortex.cycle();
 				input_status.clear();
-				let cur_frame = cur_ttl_iters as usize % 5000;
+				let cur_frame = ri.cur_ttl_iters as usize % 5000;
 				input_status.push_str(&format!("[{}] -> '{}'", cur_frame, ir_labels_vec[0]));
 			}
 
 			//let sr_start = (512 << cmn::SYNAPSES_PER_CELL_PROXIMAL_LOG2) as usize;
 
-			if !view_sdr_only {
-				print!("\n\n=== Iteration {}/{} ===", i + 1, test_iters);
+			if !ri.view_sdr_only {
+				print!("\n\n=== Iteration {}/{} ===", i + 1, ri.test_iters);
 
 				if false {
 					print!("\nSENSORY INPUT VECTOR:");
 					//ocl::fmt::print_vec(&input_czar.vec()_optical[..], 1 , None, None, false);
 				}
 
-				output_czar::print_sense_and_print(&mut cortex, &area_name);
+				output_czar::print_sense_and_print(&mut ri.cortex, &ri.area_name);
 			}
 
 			// REQUIRES cortex.area(&area_name).axns.states TO BE FILLED BY .print() unless:
 
-			if view_sdr_only { cortex.area_mut(&area_name).psal_mut().dens.states.read_wait(); }
+			if ri.view_sdr_only { ri.cortex.area_mut(&ri.area_name).psal_mut().dens.states.read_wait(); }
 
-			cortex.area_mut(&area_name).axns.states.read_wait();
+			ri.cortex.area_mut(&ri.area_name).axns.states.read_wait();
 
 			//let (eff_out_idz, eff_out_idn) = cortex.area(&area_name).mcols.axn_output_range();
 			//let (ssts_axn_idz, ssts_axn_idn) = cortex.area_mut(&area_name).psal_mut().axn_range();
@@ -458,16 +313,16 @@ pub fn run(autorun_iters: i32) -> bool {
 			//let ff_slc = &cortex.area(&area_name).axns.states.vec()[ssts_axn_idz..ssts_axn_idn];
 			//let ff_slc = &cortex.area(&area_name).psal_mut().dens.states.vec()[..];
 
-			print!("\n'{}' output:", &area_name);
+			print!("\n'{}' output:", &ri.area_name);
 
-			cortex.area_mut(&area_name).render_aff_out(&input_status, true);
+			ri.cortex.area_mut(&ri.area_name).render_aff_out(&input_status, true);
 
-			if view_all_axons {
+			if ri.view_all_axons {
 				print!("\n\nAXON SPACE:\n");
 				
 				// let axn_space_len = cortex.area(&area_name).axns.states.vec().len();
 
-				cortex.area_mut(&area_name).render_axon_space();
+				ri.cortex.area_mut(&ri.area_name).render_axon_space();
 			}			
 
 			i += 1;
@@ -479,15 +334,15 @@ pub fn run(autorun_iters: i32) -> bool {
 			}
 		}
 
-		if test_iters > 1000 {
-			test_iters = 1;
+		if ri.test_iters > 1000 {
+			ri.test_iters = 1;
 		}
 
-		if !bypass_act {
-			cur_ttl_iters += i;
+		if !ri.bypass_act {
+			ri.cur_ttl_iters += i;
 		}
 
-		if autorun_iters > 0 {
+		if ri.autorun_iters > 0 {
 			break;
 		}
 	}
@@ -497,20 +352,213 @@ pub fn run(autorun_iters: i32) -> bool {
 	true
 }
 
+struct RunInfo {
+	cortex: Cortex,
+	test_iters: i32, 
+	bypass_act: bool, 
+	autorun_iters: i32, 
+	first_run: bool, 
+	view_all_axons: bool, 
+	view_sdr_only: bool,
+	area_name: String,
+	cur_ttl_iters: i32,
+}
 
-// struct Prompt {
-// 	prompt_pre: &'static str,
-// 	prompt_post: &'static str,
-// }
+enum LoopAction {
+	Break,
+	Continue,
+	None,
+}
 
 
-// struct Command {
-// 	text: &'static str,
-// }
+fn prompt(ri: &mut RunInfo) -> LoopAction {
+	if ri.test_iters == 0 {
+		ri.test_iters = 1;
+		ri.bypass_act = true; 
+	} else {
+		ri.bypass_act = false;
+	}
+
+	if ri.autorun_iters == 0 {
+		let in_string: String = if ri.first_run {
+			ri.first_run = false;
+			"\n".to_string()
+		} else {
+			let axn_state = if ri.view_all_axons { "on" } else { "off" };
+			let view_state = if ri.view_sdr_only { "sdr" } else { "all" };
+
+			rin(format!("bismit: [{ttl_i}/({loop_i})]: [v]iew:[{}] [a]xons:[{}] \
+				[m]otor:[X] a[r]ea:[{}] [t]ests [q]uit [i]ters:[{iters}]", 
+				view_state, axn_state, /*input_czar.motor_state.cur_str(),*/ ri.area_name, 
+				iters = ri.test_iters,
+				loop_i = 0, //input_czar.counter(), 
+				ttl_i = ri.cur_ttl_iters,
+			))
+		};
+
+
+		if "q\n" == in_string {
+			print!("\nExiting interactive test mode... ");
+			// break;
+			return LoopAction::Break;
+		} else if "i\n" == in_string {
+			let in_s = rin(format!("Iterations: [i={}]", ri.test_iters));
+			if "\n" == in_s {
+				// continue;
+				return LoopAction::Continue;
+				//test_iters = TEST_ITERATIONS;
+			} else {
+				let in_int: Option<i32> = parse_num(in_s);
+				match in_int {
+					Some(x)	=> {
+						 ri.test_iters = x;
+						 //continue;
+						 // return LoopAction::Continue;
+						 return LoopAction::None;
+					},
+					None    => {
+						print!("Invalid number.\n");
+						// continue;
+						return LoopAction::Continue;
+					},
+				}
+			}
+
+		} else if "r\n" == in_string {
+			let in_str = rin(format!("area name"));
+			let in_s1 = in_str.trim();
+			let new_area_name = in_s1.to_string();
+			
+			if ri.cortex.valid_area(&new_area_name) {
+				ri.area_name = new_area_name;
+			} else {
+				print!("Invalid area.");
+			}
+			//continue;
+			ri.bypass_act = true;
+			return LoopAction::None;
+
+		} else if "v\n" == in_string {
+			ri.view_sdr_only = !ri.view_sdr_only;
+			ri.bypass_act = true;
+			return LoopAction::None;
+
+		} else if "\n" == in_string {
+			return LoopAction::None;
+			// DO NOT REMOVE
+
+		} else if "a\n" == in_string {
+			ri.view_all_axons = !ri.view_all_axons;
+			ri.bypass_act = true;
+			return LoopAction::None;
+
+		} else if "t\n" == in_string {
+			// bypass_act = true;
+			let in_s = rin(format!("tests: [f]ract [c]ycles [l]earning [a]ctivate a[r]ea_output o[u]tput"));
+
+			if "p\n" == in_s {
+				//synapse_drill_down::print_pyrs(&mut cortex);
+				// continue;
+				return LoopAction::Continue;
+
+			} else if "u\n" == in_s {
+				// let in_str = rin(format!("area name"));
+				// let in_s1 = in_str.trim();
+				// let out_len = cortex.area(&in_s).dims.columns();
+				// let t_vec: Vec<u8> = iter::repeat(0).take(out_len as usize).collect();
+				// cortex.area_mut(&in_s).read_output(&mut t_vec, map::FF_OUT);
+				// ocl::fmt::print_vec_simple(&t_vec);
+				println!("\n##### PRINTING TEMPORARILY DISABLED #####");
+				// continue;
+				return LoopAction::Continue;
+
+			} else if "c\n" == in_s {
+				println!("\n##### DISABLED #####");
+				//hybrid::test_cycles(&mut cortex, &area_name);
+				// continue;
+				return LoopAction::Continue;
+
+			} else if "l\n" == in_s {
+				println!("\n##### DISABLED #####");
+				//learning::test_learning_cell_range(&mut cortex, inhib_layer_name, &area_name);
+				// continue;
+				return LoopAction::Continue;
+
+			} else if "a\n" == in_s {
+				println!("\n##### DISABLED #####");
+				//learning::test_learning_activation(&mut cortex, &area_name);
+				// continue;
+				return LoopAction::Continue;
+
+			// } else if "f\n" == in_s {
+			// 	let in_s = rin(format!("fractal seed"));
+			// 	let in_int: Option<u8> = in_s.trim().parse().ok();
+
+			// 	// let seed = match in_int {
+			// 	// 	Some(x)	=> x,
+			// 	// 	None => {
+			// 	// 		print!("\nError parsing number.");
+			// 	// 		continue;
+			// 	// 	},
+			// 	// };
+
+			// 	let in_s = rin(format!("cardinality factor: 256 * "));
+			// 	let in_int: Option<usize> = in_s.trim().parse().ok();
+
+			// 	let c_factor = match in_int {
+			// 		Some(x)	=> x,
+			// 		None => {
+			// 			print!("\nError parsing number.");
+			// 			continue;
+			// 		},
+			// 	};
+
+			// 	// let tvec = cmn::gen_fract_sdr(seed, 256 * c_factor);
+			// 	// ocl::fmt::print_vec_simple(&tvec[..]);
+			// 	println!("\n##### PRINTING TEMPORARILY DISABLED #####");
+			// 	continue;
+
+			// } else if "r\n" == in_s {
+			// 	let in_str = rin(format!("area name"));
+			// 	// let in_s = in_str.trim();
+			// 	//let in_int: Option<u8> = in_s.trim().parse().ok();
+
+			// 	println!("\n##### DISABLED #####");
+			// 	//cortex.print_area_output(&in_s);
+			// 	continue;
+
+			} else {
+				// continue;
+				return LoopAction::Continue;
+			}
+
+
+		} else if "m\n" == in_string {
+			// bypass_act = true;
+			let in_s = rin(format!("motor: [s]witch(disconnected)"));
+			if "s\n" == in_s {
+				//input_czar.motor_state.switch();
+				//println!("\nREPLACE ME - synapse_sources::run() - line 100ish");
+				// continue;
+				return LoopAction::Continue;
+				//test_iters = TEST_ITERATIONS;
+
+			} else {
+				// continue;
+				return LoopAction::Continue;
+			}
+		} else {
+			// continue;
+			return LoopAction::Continue;
+		}
+	}
+
+	LoopAction::None
+}
 
 
 
-fn rin(prompt: String) -> String {
+pub fn rin(prompt: String) -> String {
 	let mut in_string: String = String::new();
 	print!("\n{}:> ", prompt);
 	io::stdout().flush().unwrap();
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use time::{ Timespec };
+
+use interactive::visualize;
+
 extern crate time;
 extern crate bismit;
 
@@ -7,11 +11,6 @@ mod interactive;
 fn main() {
 	println!("================= Bismit: visbis::main() running... =================");
 	let time_start = time::get_time();
-	// test_1::run_kernel();
-	// sense::ascii_sense();
-	// test_3::run();
-	// test_casting::run();
-	// hello_world::run();
 
 	// if true {
 	// 	interactive::visualize::run(0);
@@ -21,14 +20,93 @@ fn main() {
 	// 	}
 	// }
 	
-	//test_miccos::run();
-	//test_reader();
+	interactive::visualize::run(0);
 
-	interactive::visualize::run(0);	
+	// tomfoolery(&time_start);
 
-	// <<<<< MOVE THIS TO CMN AND MAKE A FUNCTION FOR IT >>>>>
+
+	// <<<<< MOVE THIS ELSEWHERE >>>>>
 	let time_complete = time::get_time() - time_start;
 	let t_sec = time_complete.num_seconds();
 	let t_ms = time_complete.num_milliseconds() - (t_sec * 1000);
 	println!("\n====== Bismit: visbis::main() complete in: {}.{} seconds ======", t_sec, t_ms);
 }
+
+
+#[allow(dead_code)]
+fn tomfoolery(ts: &Timespec) {
+	use std::thread::{ self, JoinHandle };
+	use std::sync::{ Arc, Mutex };
+	use std::sync::mpsc;
+	use std::time::{ Duration };
+
+	const TCOUNT: usize = 3;
+
+	let mut handles = Vec::<JoinHandle<_>>::with_capacity(10);
+	let data = Arc::new(Mutex::new(0));
+    let (tx, rx) = mpsc::channel();
+
+    for h_id in 0..TCOUNT {
+        let (data, tx) = (data.clone(), tx.clone());
+
+		handles.push(thread::Builder::new().name(h_id.to_string()).spawn(move || {
+
+        // handles.push(thread::spawn(move || {
+            let mut data = match data.lock() {
+            	Ok(d) => d,
+            	Err(err) => {
+            		println!("Error locking mutex: '{:?}'", err);
+            		tx.send((h_id, time::get_time(), "ERROR".to_string())).ok();
+            		return;
+        		},
+            };
+
+            *data += 1;            
+
+            thread::sleep(Duration::new(0, h_id as u32 * 10000000));
+
+            let msg = visualize::rin(h_id.to_string());
+
+            tx.send((h_id, time::get_time(), msg.clone())).ok();
+
+            if msg.trim() == "" { panic!("Empty!"); }
+        }).expect("Error creating thread"));
+    }
+
+    for i in 0..TCOUNT {
+        let (t_id, tf, msg) = match rx.recv() {
+        	Ok(r) => r,
+        	Err(err) => {
+        		println!("Receive error: '{:?}'", err);
+        		return;
+    		},
+    	};
+
+        let elpsd = (tf - *ts).num_milliseconds();
+
+        println!("[msg: {}]: thread {} finished after {}ms with message: '{}'", i, t_id, elpsd, msg.trim());
+    }
+
+    print!("\n");
+
+    for i in 0..TCOUNT {
+    	let rslt = match handles.pop() {
+    		Some(r) => r.join(),
+    		None => {
+    			println!("Thread {} failed.", i);
+    			break;
+			}
+		};
+
+    	println!("Result of thread {} is '{:?}'", i, rslt);
+	}
+
+    println!("\n{:?}", data);
+}
+
+
+
+// fn ms_since(start: &Timespec) -> i64 {
+// 	let time_elapsed = time::get_time() - *start;
+// 	time_elapsed.num_milliseconds()
+// }
